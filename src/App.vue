d3709244ed8fe5c1674cda013a2e4045bcd5d07f
Vitaly Mosin <mosinve@gmail.com>
2018-07-12 17:51:57 +0700

    Fixes

--- src/App.vue
+++ src/App.vue
@@ -18,56 +18,56 @@
 </template>
 
 <script>
-  import SourceList from './components/SourceList'
-import sourceData from './sourceData'
-import MapCmp from './components/MapCmp'
+import SourceList from "./components/SourceList";
+import sourceData from "./sourceData";
+import MapCmp from "./components/MapCmp";
+import "leaflet/dist/leaflet.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 export default {
-  name: 'app',
-  components: {MapCmp, SourceList },
+  name: "app",
+  components: { MapCmp, SourceList },
   data() {
     return {
       sourceData,
       selected: []
-    }
+    };
   },
 
   methods: {
     onSelected(data) {
-      this.selected.push(data)
+      this.selected.push(data);
     },
     onAdded(e) {
-      const clickedEl = e.target.__vue__
-      if (clickedEl.selected || clickedEl.added) return
-      if(clickedEl.$children.length) {
-        clickedEl.selected = true
-        clickedEl.$children.forEach(child=>child.$el.click())
+      const clickedEl = e.target.__vue__;
+      if (clickedEl.selected || clickedEl.added) return;
+      if (clickedEl.$children.length) {
+        clickedEl.selected = true;
+        clickedEl.$children.forEach(child => child.$el.click());
       }
-      if(clickedEl.item.coords) {
-        this.$refs.map.addMarker(clickedEl)
-        clickedEl.added = true
+      if (clickedEl.item.coords) {
+        this.$refs.map.addMarker(clickedEl);
+        clickedEl.added = true;
       }
     },
     remove(target) {
-      const currentItem = this.selected[target]
-      if (currentItem.$parent.selected){
-        this.$refs.map.addMarker(currentItem)
-        currentItem.added = true
+      const currentItem = this.selected[target];
+      if (currentItem.$parent.selected) {
+        this.$refs.map.addMarker(currentItem);
+        currentItem.added = true;
       } else {
-        currentItem.added = false
+        currentItem.added = false;
       }
 
-     this.selected = this.selected.filter((item, index) => index !== target)
+      this.selected = this.selected.filter((item, index) => index !== target);
     }
   }
-}
+};
 </script>
 
 <style lang='scss'>
-@import "~leaflet/dist/leaflet.css";
-@import "~bootstrap/scss/bootstrap";
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
@@ -88,5 +88,4 @@ export default {
 #selection {
   grid-area: selection;
 }
-
 </style>
